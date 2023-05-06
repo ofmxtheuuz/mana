0506ac61b1f96aea2d34b3321b7e0fdd2efaeef7
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-05-05 22:31:11 -0300

    Form finished!

--- src/components/sections/Create.tsx
+++ src/components/sections/Create.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 export default function Create() {
+
+  const [title, setTitle] = useState<string>("")
+  const [description, setDescription] = useState<string>("")
+
+  const submit = () => {
+    if(title.length > 0 && description.length > 2) {  
+      console.log(`Formulário recebido: título: ${title}, descrição: ${description}`)
+    }
+  }
 
   return <View style={s.box}>
     <Text style={s.title}>
       Criar item
     </Text>
+    <SafeAreaView>
+      <TextInput style={s.input} placeholder="Título do item" placeholderTextColor={"#FFFFFF"} onChangeText={(txt: string) => setTitle(txt)}/>
+      <TextInput style={s.input} placeholder="Descrição do item" placeholderTextColor={"#FFFFFF"} onChangeText={(txt: string) => setDescription(txt)}/>
+      <TouchableOpacity style={s.button} onPress={submit}>
+        <Text style={{textAlign: "center", color: "#FFF"}}>
+          Salvar
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   </View>
 }
 
@@ -18,8 +36,28 @@ const s = StyleSheet.create({
   },
   title: {
     color: "#FFFFFF",
-    marginTop: 15,
+    marginVertical: 10,
     marginLeft: 15,
-    fontSize: 17
+    fontSize: 17,
+  },
+  input: {
+    color: "#FFF",
+    marginLeft: 15,
+    height: 40,
+    width: 380,
+    margin: 5,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 6,
+    borderColor: "#FFF"
+  },
+  button: {
+    marginTop: 6,
+    borderWidth: 1,
+    borderColor: "#FFF",
+    margin: 15,
+    width: 380,
+    paddingVertical: 5,
+    borderRadius: 6 
   }
 })
