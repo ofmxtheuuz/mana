d9c0b4203d91da10f34c0411f4c47bb981630f8d
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-05-06 09:20:53 -0300

    Finished app. Needs uptade to useState all items

--- src/components/ShowItem.tsx
+++ src/components/ShowItem.tsx
@@ -1,19 +1,30 @@
 ﻿import Item from "../models/Item";
 import {StyleSheet, Text, View} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ItensRepository from "../repositories/ItensRepository";
 
 interface ShowItemProps {
     item: Item
 }
 
 export default function ShowItem({item}: ShowItemProps) {
-    return <View style={s.box} key={item.id}>
-        <Text style={{color: "rgba(153,0,255,0.5)"}}>
-            {item.title}
-        </Text>
-        <Text style={s.description}>
-            {item.description.slice(0, 60)}
-        </Text>
+    
+    const updateItem = (id: string) => {
+        ItensRepository.changeChecked(id)
+    }
+    
+    return <View style={s.box} >
+        <View>
+            <Text style={{color: "rgba(153,0,255,0.5)"}}>
+                {item.title}
+            </Text>
+            <Text style={s.description}>
+                {item.description.slice(0, 60)}
+            </Text>
+        </View>
+        {item.isChecked ? <Icon onPress={() => {updateItem(item.id)}} name="check-square-o" size={30} color="purple" /> : <Icon onPress={() => {updateItem(item.id)}} name="square-o" size={30} color="purple" />}
+        
+
     </View>
 }
 
@@ -24,7 +35,11 @@ const s = StyleSheet.create({
         borderColor: "#9900ff",
         borderRadius: 6,
         marginBottom: 15,
-        padding: 15
+        padding: 15,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
     },
     description: {
         maxWidth: 290,
--- src/components/sections/Items.tsx
+++ src/components/sections/Items.tsx
@@ -14,6 +14,6 @@ export default function Items() {
     }, [])
     
     return <Container>
-        {allItems.length > 0 && allItems.map((item: Item) => <ShowItem item={item} />)}
+        {allItems.length > 0 && allItems.map((item: Item) => <ShowItem key={item.id} item={item} />)}
     </Container>
 }
